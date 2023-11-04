afc1ecec71fbf401212cc3376f27d05f0945e1a6
viiniii <123756468+viiniiii@users.noreply.github.com>
2023-11-04 20:14:49 +0100

    Delete help.py

--- help.py
+++
@@ -1,8 +0,0 @@
-with open(".\Don'tTouchMyPresents\Don'tTouchMyPresents", 'rb') as file:
-    # Read the binary data from the file
-    binary_data = file.read()
-
-# Now, you can work with the binary data as needed
-# For example, you can print the binary data as a hexadecimal string
-hex_string = binary_data.hex()
-print(hex_string)
